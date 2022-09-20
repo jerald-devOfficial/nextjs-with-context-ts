e3cc87b7e1635c6643132cc6bf05113da694532f
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2022-09-21 02:12:18 +0800

    Changes to cart.tsx, render cart orders

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,17 +1,31 @@
-import items from "../data/items";
+import data from "../data/items";
+import { useContext } from "react";
+import ShoppingCartContext from "../components/context/cartContext";
+import { Reduce } from "../interfaces";
 
-type Props = {};
+const getFullItem = (id: string) => {
+  const idx = data.findIndex((item) => item.id === id);
+  return data[idx];
+};
 
-const Cart = (props: Props) => {
+const Cart = (): JSX.Element => {
+  const { items }: any = useContext(ShoppingCartContext);
+  const total: number = Object.keys(items)
+    .map((id: string) => getFullItem(id).price * items[id])
+    .reduce((x: number, y: number) => x + y, 0);
+  const amounts = Object.keys(items).map((id) => {
+    const item = getFullItem(id);
+    return { item, amount: items[id] };
+  });
   return (
     <div>
-      <h1 className="text-xl font-bold"> Total: ${/* To be implemented */} </h1>
+      <h1 className="text-xl font-bold"> Total: ${total} </h1>
       <div>
-        {/* {items.map(({ item, amount }) => (
-        <div key={item.id}>
-          x{amount} {item.name} (${amount * item.price})
-        </div>
-      ))} */}
+        {amounts.map(({ item, amount }) => (
+          <div key={item.id}>
+            x{amount} {item.name} (${amount * item.price})
+          </div>
+        ))}
       </div>
     </div>
   );
